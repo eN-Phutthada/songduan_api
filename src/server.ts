75d5ed4b269ab61546e9d94774e85754f5ce9493
eN-Phutthada <66011212119@msu.ac.th>
2025-10-08 20:34:30 +0700

    Test2

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,13 @@
 import http, { Server } from "http";
 import { app } from "./app";
 
-const PORT: number = Number(process.env.PORT) || 3000;
-const HOST = process.env.HOST || "0.0.0.0";
-
 const server: Server = http.createServer(app);
 
-server.listen(PORT, HOST, () => {
-    console.log(`🚀 Server running at http://${HOST}:${PORT}`);
+server.listen(() => {
+    console.log(`🚀 Server running at https://songduan-api.onrender.com`);
 });
 
-server.on("error", onError);
 server.on("listening", onListening);
-
-function onError(error: NodeJS.ErrnoException): void {
-    if (error.syscall !== "listen") throw error;
-    const bind = `Port ${PORT}`;
-    switch (error.code) {
-        case "EACCES":
-            console.error(`${bind} requires elevated privileges`);
-            process.exit(1);
-        case "EADDRINUSE":
-            console.error(`${bind} is already in use`);
-            process.exit(1);
-        default:
-            throw error;
-    }
-}
 
 function onListening(): void {
     const addr = server.address();
