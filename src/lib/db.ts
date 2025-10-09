e2e79236ad6a6267394409f4d61645fd152b95f2
eN-Phutthada <66011212119@msu.ac.th>
2025-10-09 23:16:16 +0700

    Test ลด Error: read ECONNRESET

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,6 +8,12 @@ export const conn = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
     waitForConnections: true,
     connectionLimit: 10,
+    queueLimit: 0,
+
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+    connectTimeout: 20000,
 });
